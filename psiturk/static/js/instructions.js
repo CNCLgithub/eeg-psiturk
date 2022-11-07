89a29dec0472ca75f6daafd5c43fb5dc39384ece
Chloe Geller <chloegeller@users.noreply.github.com>
2022-11-07 13:24:10 -0500

    commented out some instructions

--- psiturk/static/js/instructions.js
+++ psiturk/static/js/instructions.js
@@ -6,7 +6,7 @@
 // 5: Any delay added to progression
 
 var EXP_DURATION = 20;
-var N_TRIALS = 120;
+var N_TRIALS = 10;
 
 var instructions = [
     [
@@ -32,44 +32,48 @@ var instructions = [
         "Click <b>Switch to full screen</b> and then <b>Next</b> to continue.",
         "fullscreen", "", false, 0
     ],
-    [
-        "The study is designed to be <i>challenging</i>. Sometimes, you'll be certain about what you saw. Other times, you won't be -- and this is okay! Just give your best guess each time.",
-        "", "", false, 3
-    ],
-    [
-        `I know it is also difficult to stay focused for so long, especially when you are doing the same thing over and over. But remember, the experiment will be all over in less than ${EXP_DURATION} minutes. Please do your best to remain focused! Your responses will only be useful to me if you remain focused.`,
-        "", "", false, 3
-    ],
+    // [
+    //     "The study is designed to be <i>challenging</i>. Sometimes, you'll be certain about what you saw. Other times, you won't be -- and this is okay! Just give your best guess each time.",
+    //     "", "", false, 3
+    // ],
+    // [
+    //     `I know it is also difficult to stay focused for so long, especially when you are doing the same thing over and over. But remember, the experiment will be all over in less than ${EXP_DURATION} minutes. Please do your best to remain focused! Your responses will only be useful to me if you remain focused.`,
+    //     "", "", false, 3
+    // ],
     [
         "<b>Before we begin, follow the instructions below to setup your display.</b><br><hr />" +
         "<p>Please sit comfortably in front of you monitor and outstretch your arm holding a credit card (or a similary sized ID card). <br>" +
         "<p>Adjust the size of the image using the slider until its <strong>width</strong> matches the width of your credit card (or ID card).",
         "scale", "generic_cc.png", false, 1
     ],
+    // -------
     //[
         //"Please adjust contrast with the slider below so that you can see 7 rectangles as well as possible.",
         //"contrast", "contrast.png", false, 0
     //],
+    // --------
+
     // image with target labels (blue)
-    [
-        "At the beginning of each instance of the task, you will briefly see two images of rooms, with a white flash in between.<br> "+
-            "Your task is to determine wether the two scene images where identical or different in any way.<br>" +
-            "Click <b>Next</b> to see an example video.",
-        "image", "1.png", false, 3
-    ],
-    [
-        "", "movie", "1_1_1.mp4", false, 0
-    ],
-    [
-        "At the end of each instance of the task, please record your response with your key board.<br>" +
-            "You will not be able to progress untill you make a selection.<br>" +
-            "Click <b>Next</b> to give it a try.",
-        "", "", false, 3
-    ],
-    [
-        "",
-        "movie", "1_1_1.mp4", true, 0
-    ],
+
+    // [
+    //     "At the beginning of each instance of the task, you will briefly see two images of rooms, with a white flash in between.<br> "+
+    //         "Your task is to determine wether the two scene images where identical or different in any way.<br>" +
+    //         "Click <b>Next</b> to see an example video.",
+    //     "image", "1.png", false, 3
+    // ],
+    // [
+    //     "", "movie", "1_1_1.mp4", false, 0
+    // ],
+    // [
+    //     "At the end of each instance of the task, please record your response with your key board.<br>" +
+    //         "You will not be able to progress untill you make a selection.<br>" +
+    //         "Click <b>Next</b> to give it a try.",
+    //     "", "", false, 3
+    // ],
+    // [
+    //     "",
+    //     "movie", "1_1_1.mp4", true, 0
+    // ],
     [
         `Please maintain this arm-length distance from your monitor for the duration of this experiment (${EXP_DURATION} minutes).` +
             "<br> Also, please keep your hands near the <b>f and j</b> keys for comfort and accuracy.",
